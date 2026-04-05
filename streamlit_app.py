07f75e8637d8229be29c2cbdd799f3bf9fbfbc4d
DwiKrwn <dwikwrn12@gmail.com>
2026-04-05 08:59:17 +0700

    Fix input for height to allow zero value

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Menghitung Volume :blue[Tabung] :sunglasses:")
 
 r = st.number_input("Masukan Jari-Jari (cm):",0)
-t = st.number_input("Masukan Tinggi (cm)")
+t = st.number_input("Masukan Tinggi (cm)", 0)
