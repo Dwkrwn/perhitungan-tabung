698205e399080ebd8f545f5f6988677e11145385
DwiKrwn <dwikwrn12@gmail.com>
2026-04-05 09:03:31 +0700

    Add volume calculation for cylinder in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ st.title("Menghitung Volume :blue[Tabung] :sunglasses:")
 
 r = st.number_input("Masukan Jari-Jari (cm):",0)
 t = st.number_input("Masukan Tinggi (cm)", 0)
+
+if st.button("Hitung Volume", type="primary"):
+  v = math.pi(r**2)*t
+  st.success(f"Volume Tabung adalah {v:.2f}")
